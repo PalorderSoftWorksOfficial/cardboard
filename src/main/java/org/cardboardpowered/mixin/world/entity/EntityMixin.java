0d3830e09d21a097f340a5a38ebce37385ae05a0
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-06-08 10:47:35 +0200

    Fix entity air supply hook to modify argument

--- src/main/java/org/cardboardpowered/mixin/world/entity/EntityMixin.java
+++ src/main/java/org/cardboardpowered/mixin/world/entity/EntityMixin.java
@@ -1,25 +1,6 @@
-/**
- * Cardboard - Spigot/Paper for Fabric
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
 package org.cardboardpowered.mixin.world.entity;
 
-import org.cardboardpowered.bridge.commands.CommandSourceBridge;
-import org.cardboardpowered.bridge.world.entity.EntityBridge;
-// import com.llamalad7.mixinextras.sugar.Local;
+import java.util.ArrayList;
 
 import me.isaiah.common.entity.IRemoveReason;
 import net.minecraft.commands.CommandSourceStack;
@@ -33,14 +14,13 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.storage.ValueOutput;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.CraftServer;
-import org.bukkit.craftbukkit.entity.*;
+import org.bukkit.craftbukkit.entity.CraftEntity;
 import org.bukkit.entity.Pose;
 import org.bukkit.event.entity.EntityAirChangeEvent;
 import org.bukkit.event.entity.EntityCombustByBlockEvent;
@@ -48,23 +28,32 @@ import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.entity.EntityPoseChangeEvent;
 import org.bukkit.projectiles.ProjectileSource;
+import org.cardboardpowered.bridge.commands.CommandSourceBridge;
+import org.cardboardpowered.bridge.world.entity.EntityBridge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.ArrayList;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin implements CommandSourceBridge, EntityBridge {
 
     public CraftEntity bukkitEntity;
-    public org.bukkit.projectiles.ProjectileSource projectileSource;
-    private ArrayList<org.bukkit.inventory.ItemStack> drops = new ArrayList<org.bukkit.inventory.ItemStack>();
+    public ProjectileSource projectileSource;
+    private ArrayList<org.bukkit.inventory.ItemStack> drops = new ArrayList<>();
     private boolean forceDrops;
+    public boolean valid = false;
+    public boolean cardboard$inWorld = false;
+    public Location origin_bukkit;
+
+    @Shadow
+    private Level level;
+
+    @Shadow
+    private EntityDimensions dimensions;
 
     @Override
     public ArrayList<org.bukkit.inventory.ItemStack> cardboard_getDrops() {
@@ -75,46 +64,26 @@ public abstract class EntityMixin implements CommandSourceBridge, EntityBridge {
     public void cardboard_setDrops(ArrayList<org.bukkit.inventory.ItemStack> drops) {
         this.drops = drops;
     }
-    
+
     @Override
     public AABB cardboad_getBoundingBoxAt(double x2, double y2, double z2) {
         return this.dimensions.makeBoundingBox(x2, y2, z2);
     }
 
     @Override
-    public boolean cardboard_getForceDrops() {return forceDrops;}
+    public boolean cardboard_getForceDrops() {
+        return forceDrops;
+    }
 
     @Override
     public void cardboard_setForceDrops(boolean forceDrops) {
         this.forceDrops = forceDrops;
     }
 
-    @Shadow
-    private Level level;
-    
     @Override
     public Level mc_world() {
-    	return level;
+        return level;
     }
-
-    @Shadow
-    private EntityDimensions dimensions;
-
-    public EntityMixin() {
-    }
-
-    /*
-    public void sendSystemMessage(Text message) {
-        // TODO: 1.19
-    	
-    	((Entity) (Object) this).sendMessage(message);
-    	//((Entity) (Object) this).sendSystemMessage(message, UUID.randomUUID());
-    }
-    */
-
-    public boolean valid = false;
-    public boolean cardboard$inWorld = false;
-    public Location origin_bukkit;
 
     @Override
     public Location getOriginBF() {
@@ -136,34 +105,28 @@ public abstract class EntityMixin implements CommandSourceBridge, EntityBridge {
         this.valid = b;
     }
 
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"),
-    		method = "spawnAtLocation(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/phys/Vec3;)Lnet/minecraft/world/entity/item/ItemEntity;")
-    		// method = "dropStack(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/item/ItemStack;F)Lnet/minecraft/entity/ItemEntity;")
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"), method = "spawnAtLocation(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/phys/Vec3;)Lnet/minecraft/world/entity/item/ItemEntity;")
     public boolean cardboard$mixinEntity_dropStack_EntityDropItemEvent(ServerLevel world, Entity entity, ServerLevel sworld, ItemStack itemstack, Vec3 offset) {
-        if (itemstack.isEmpty())
+        if (itemstack.isEmpty()) {
             return false;
-
-        boolean chick = (((Entity)(Object)this) instanceof Chicken && itemstack.getItem() == Items.EGG);
-        if (((Entity)(Object)this) instanceof net.minecraft.world.entity.LivingEntity && !this.forceDrops) {
-            if (!chick) {
-                this.drops.add(org.bukkit.craftbukkit.inventory.CraftItemStack.asBukkitCopy(itemstack));
-                return false;
-            }
         }
-        ItemEntity entityitem = new ItemEntity(this.level,
-        		((Entity) (Object) this).getX() + offset.x,
-        		((Entity) (Object) this).getY() + offset.y,
-        		((Entity) (Object) this).getZ() + offset.z, itemstack);
 
+        boolean chick = ((Entity) (Object) this) instanceof Chicken && itemstack.getItem() == Items.EGG;
+        if (((Entity) (Object) this) instanceof net.minecraft.world.entity.LivingEntity && !this.forceDrops && !chick) {
+            this.drops.add(org.bukkit.craftbukkit.inventory.CraftItemStack.asBukkitCopy(itemstack));
+            return false;
+        }
+
+        ItemEntity entityitem = new ItemEntity(this.level, ((Entity) (Object) this).getX() + offset.x, ((Entity) (Object) this).getY() + offset.y, ((Entity) (Object) this).getZ() + offset.z, itemstack);
         entityitem.setDefaultPickUpDelay();
 
-        EntityDropItemEvent event = new EntityDropItemEvent(this.getBukkitEntity(), (org.bukkit.entity.Item) ((EntityBridge)entityitem).getBukkitEntity());
+        EntityDropItemEvent event = new EntityDropItemEvent(this.getBukkitEntity(), (org.bukkit.entity.Item) ((EntityBridge) entityitem).getBukkitEntity());
         Bukkit.getPluginManager().callEvent(event);
-        if (event.isCancelled())
+        if (event.isCancelled()) {
             return false;
+        }
         return this.level.addFreshEntity(entityitem);
     }
-
 
     @Override
     public CommandSender getBukkitSender(CommandSourceStack serverCommandSource) {
@@ -172,34 +135,30 @@ public abstract class EntityMixin implements CommandSourceBridge, EntityBridge {
 
     @Override
     public CraftEntity getBukkitEntityRaw() {
-    	return bukkitEntity;
+        return bukkitEntity;
     }
-    
+
     @Override
-    public org.bukkit.craftbukkit.entity.CraftEntity getBukkitEntity() {
+    public CraftEntity getBukkitEntity() {
         if (this.bukkitEntity == null) {
-            // Paper start - Folia schedulers
             synchronized (this) {
                 if (this.bukkitEntity == null) {
-                    return this.bukkitEntity = org.bukkit.craftbukkit.entity.CraftEntity.getEntity(CraftServer.INSTANCE, (Entity) (Object) this);
+                    return this.bukkitEntity = CraftEntity.getEntity(CraftServer.INSTANCE, (Entity) (Object) this);
                 }
             }
-            // Paper end - Folia schedulers
         }
         return this.bukkitEntity;
     }
 
     @Inject(at = @At("HEAD"), method = "restoreFrom(Lnet/minecraft/world/entity/Entity;)V")
     public void cardboard$setBukkitHandleForCopy(Entity entity, CallbackInfo ci) {
-        // Paper start - Forward CraftEntity in teleport command
-        org.bukkit.craftbukkit.entity.CraftEntity bukkitEntity = ((EntityBridge)entity).getBukkitEntityRaw();
+        CraftEntity bukkitEntity = ((EntityBridge) entity).getBukkitEntityRaw();
         if (bukkitEntity != null) {
-            bukkitEntity.setHandle((Entity)(Object)this);
+            bukkitEntity.setHandle((Entity) (Object) this);
             this.bukkitEntity = bukkitEntity;
         }
-        // Paper end - Forward CraftEntity in teleport command
     }
-    
+
     @Override
     public void setProjectileSourceBukkit(ProjectileSource source) {
         this.projectileSource = source;
@@ -212,10 +171,11 @@ public abstract class EntityMixin implements CommandSourceBridge, EntityBridge {
 
     @Inject(at = @At("HEAD"), method = "setPose(Lnet/minecraft/world/entity/Pose;)V", cancellable = true)
     public void setPoseBF(net.minecraft.world.entity.Pose entitypose, CallbackInfo ci) {
-        if (entitypose == ((Entity)(Object)this).getPose()) {
+        if (entitypose == ((Entity) (Object) this).getPose()) {
             ci.cancel();
             return;
         }
+
         Pose b = Pose.STANDING;
         switch (entitypose) {
             case CROUCHING:
@@ -243,42 +203,32 @@ public abstract class EntityMixin implements CommandSourceBridge, EntityBridge {
                 b = Pose.SWIMMING;
                 break;
             default:
-                break;  
+                break;
         }
         Bukkit.getPluginManager().callEvent(new EntityPoseChangeEvent(this.getBukkitEntity(), b));
     }
 
-    @Inject(at = @At("HEAD"), method = "setAirSupply", cancellable = true)
-    public void setAirBF(int i, CallbackInfo ci) {
+    @ModifyVariable(method = "setAirSupply", at = @At("HEAD"), argsOnly = true)
+    public int setAirBF(int i) {
         if (!valid) {
-            ci.cancel();
-            return;
+            return i;
         }
 
         EntityAirChangeEvent event = new EntityAirChangeEvent(this.getBukkitEntity(), i);
         event.getEntity().getServer().getPluginManager().callEvent(event);
-
         if (event.isCancelled()) {
-            ci.cancel();
-            return;
+            return i;
         }
-        i = event.getAmount();
+        return event.getAmount();
     }
 
     public void removeBF() {
-        ((me.isaiah.common.cmixin.IMixinEntity)this).Iremove(IRemoveReason.DISCARDED);
+        ((me.isaiah.common.cmixin.IMixinEntity) this).Iremove(IRemoveReason.DISCARDED);
     }
 
     @Shadow
     public void move(MoverType moveType, Vec3 vec3d) {
     }
-    
-    /*
-    @Shadow
-    private TeleportTarget getTeleportTarget(ServerWorld w) {
-        return null;
-    }
-    */
 
     @Shadow
     public boolean isPushable() {
@@ -298,12 +248,6 @@ public abstract class EntityMixin implements CommandSourceBridge, EntityBridge {
         return 0;
     }
 
-    /**
-     * EntityCombustByBlockEvent
-     * 
-     * @author Arclight
-     * @author Cardboard
-     */
     @Redirect(method = "lavaIgnite", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;igniteForSeconds(F)V"))
     public void cardboard$mixinEntity_igniteByLava_EntityCombustByBlockEvent(Entity entity, float seconds) {
         if ((Object) this instanceof LivingEntity && ((Entity) (Object) this).remainingFireTicks <= 0) {
@@ -311,11 +255,11 @@ public abstract class EntityMixin implements CommandSourceBridge, EntityBridge {
             org.bukkit.entity.Entity damagee = this.getBukkitEntity();
             EntityCombustEvent combustEvent = new EntityCombustByBlockEvent(damager, damagee, 15);
             Bukkit.getPluginManager().callEvent(combustEvent);
-
-            if (!combustEvent.isCancelled())
+            if (!combustEvent.isCancelled()) {
                 ((Entity) (Object) this).igniteForSeconds(combustEvent.getDuration());
+            }
         } else {
-            // This will be called every single tick the entity is in lava, so don't throw an event
             ((Entity) (Object) this).igniteForSeconds(15);
         }
     }
+}
