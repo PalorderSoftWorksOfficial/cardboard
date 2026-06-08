f3ca114b483692d78b22d11457660924eacb8242
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-06-08 10:46:55 +0200

    Clean up server level mixin stubs

--- src/main/java/org/cardboardpowered/mixin/server/level/ServerLevelMixin.java
+++ src/main/java/org/cardboardpowered/mixin/server/level/ServerLevelMixin.java
@@ -4,24 +4,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Executor;
 
-import net.minecraft.world.level.storage.LevelData;
-import org.bukkit.craftbukkit.CraftServer;
-import org.cardboardpowered.CardboardConfig;
-import org.cardboardpowered.bridge.world.level.storage.LevelData_RespawnDataBridge;
-import org.cardboardpowered.bridge.world.level.storage.PrimaryLevelDataBridge;
-import org.cardboardpowered.impl.world.CraftWorld;
-import org.cardboardpowered.bridge.server.level.ServerLevelBridge;
-import org.cardboardpowered.mixin.world.level.LevelMixin;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import org.cardboardpowered.CardboardMod;
 import com.javazilla.bukkitfabric.Utils;
-import org.cardboardpowered.bridge.world.level.LevelBridge;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
@@ -35,54 +18,49 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.entity.LevelEntityGetter;
 import net.minecraft.world.level.entity.PersistentEntitySectionManager;
+import net.minecraft.world.level.storage.LevelData;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.ServerLevelData;
+import org.bukkit.craftbukkit.CraftServer;
+import org.cardboardpowered.CardboardConfig;
+import org.cardboardpowered.CardboardMod;
+import org.cardboardpowered.bridge.server.level.ServerLevelBridge;
+import org.cardboardpowered.bridge.world.level.storage.LevelData_RespawnDataBridge;
+import org.cardboardpowered.bridge.world.level.storage.PrimaryLevelDataBridge;
+import org.cardboardpowered.mixin.world.level.LevelMixin;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLevel.class)
 public class ServerLevelMixin extends LevelMixin implements ServerLevelBridge {
 
-   // @Shadow
-   // public boolean inEntityTick;
+    private LevelStorageSource.LevelStorageAccess cardboard$session;
+    private UUID cardboard$uuid;
+    private LevelLoadListener cardboard$levelLoadListener;
 
-    /*@Inject(at = @At("TAIL"), method = "<init>")
-    public void addToBukkit( ... ,  CallbackInfo ci){
-        // ((CraftServer)Bukkit.getServer()).addWorldToMap(getCraftWorld());
-    }*/
-
-	private LevelStorageSource.LevelStorageAccess cardboard$session;
-	private UUID cardboard$uuid;
-	private LevelLoadListener cardboard$levelLoadListener;
-
-	@Inject(method = "<init>", at = @At(value = "RETURN"))
-    private void banner$initWorldServer(
-    		MinecraftServer minecraftserver,
-    		Executor executor,
-    		LevelStorageSource.LevelStorageAccess convertable_conversionsession,
-    		ServerLevelData iworlddataserver, ResourceKey<Level> resourcekey,
-    		LevelStem worlddimension, // WorldGenerationProgressListener worldloadlistener,
-    		boolean flag, long i2, List<CustomSpawner> list, boolean flag1,
-    		RandomSequences randomsequences, CallbackInfo ci
-    	) {
-		
-		if (CardboardConfig.DEBUG_OTHER) {
-			CardboardMod.LOGGER.info("Debug: getting world uuid");
-		}
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void banner$initWorldServer(MinecraftServer minecraftserver, Executor executor, LevelStorageSource.LevelStorageAccess convertable_conversionsession, ServerLevelData iworlddataserver, ResourceKey<Level> resourcekey, LevelStem worlddimension, boolean flag, long i2, List<CustomSpawner> list, boolean flag1, RandomSequences randomsequences, CallbackInfo ci) {
+        if (CardboardConfig.DEBUG_OTHER) {
+            CardboardMod.LOGGER.info("Debug: getting world uuid");
+        }
 
         this.cardboard$session = convertable_conversionsession;
         this.cardboard$uuid = Utils.getWorldUUID(cardboard$session.getDimensionPath(((ServerLevel)(Object)this).dimension()).toFile());
-        
-        // TODO: add ServerWorld argument to LoggingChunkLoadProgress constructor
         this.cardboard$levelLoadListener = new LoggingLevelLoadListener(false);
     }
 
-    @Inject(at = @At("HEAD"), method = "save")
+    @Inject(method = "save", at = @At("HEAD"))
     public void doWorldSaveEvent(ProgressListener aa, boolean bb, boolean cc, CallbackInfo ci) {
         if (!cc) {
-            org.bukkit.Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.WorldSaveEvent(this.cardboard$getWorld())); // WorldSaveEvent
+            org.bukkit.Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.WorldSaveEvent(this.cardboard$getWorld()));
         }
     }
-    
-    @Shadow 
+
+    @Shadow
     public ServerLevelData serverLevelData;
 
     @Override
@@ -90,87 +68,51 @@ public class ServerLevelMixin extends LevelMixin implements ServerLevelBridge {
         return serverLevelData;
     }
 
-	@Override
-	public CraftServer getCraftServer() {
-		// TODO Auto-generated method stub
-		return CraftServer.INSTANCE;
-	}
-	
-	@Shadow
-	private PersistentEntitySectionManager<Entity> entityManager;
+    @Override
+    public CraftServer getCraftServer() {
+        return CraftServer.INSTANCE;
+    }
 
-	@Shadow
-	public LevelEntityGetter<Entity> getEntities() {
-		return this.entityManager.getEntityGetter();
-	}
+    @Shadow
+    private PersistentEntitySectionManager<Entity> entityManager;
 
-	@Shadow
-	@Final
-	private MinecraftServer server;
+    @Shadow
+    public LevelEntityGetter<Entity> getEntities() {
+        return this.entityManager.getEntityGetter();
+    }
 
-	@Override
-	public void cardboard$set_uuid(UUID id) {
-		this.cardboard$uuid = id;
-	}
+    @Shadow
+    @Final
+    private MinecraftServer server;
 
-	@Override
-	public UUID cardboard$get_uuid() {
-		return this.cardboard$uuid;
-	}
-	
-	@Override
-	public LevelLoadListener cardboard$levelLoadListener() {
-		return cardboard$levelLoadListener;
-	}
+    @Override
+    public void cardboard$set_uuid(UUID id) {
+        this.cardboard$uuid = id;
+    }
 
-    /**
-     * @reason MapInitalizeEvent
-     * @author BukkitFabricMod
-     */
-    //@Overwrite
-   // public MapState getMapState(String s) {
-        // TODO 1.17ify
-       // return null; return (MapState) CraftServer.INSTANCE.getServer().getOverworld().getPersistentStateManager().get(() -> {
-           /*MapState newMap = new MapState(s);
-            MapInitializeEvent event = new MapInitializeEvent(((IMixinMapState)newMap).getMapViewBF());
-            Bukkit.getServer().getPluginManager().callEvent(event);
-            return newMap;
-        }, s);*/
-  //  }
+    @Override
+    public UUID cardboard$get_uuid() {
+        return this.cardboard$uuid;
+    }
 
-    // TODO 1.17ify
-   /* @Inject(at = @At("TAIL"), method = "unloadEntity")
-    public void unvalidateEntityBF(Entity entity, CallbackInfo ci) {
-        ((IMixinEntity)entity).setValid(false);
-    } 
+    @Override
+    public LevelLoadListener cardboard$levelLoadListener() {
+        return cardboard$levelLoadListener;
+    }
 
-    @Inject(at = @At("TAIL"), method = "loadEntityUnchecked")
-    public void validateEntityBF(Entity entity, CallbackInfo ci) {
-        //if (!this.inEntityTick) {
-            IMixinEntity bf = (IMixinEntity) entity;
-            bf.setValid(true);
-            if (null == bf.getOriginBF() && null != bf.getBukkitEntity()) {
-                // Paper's Entity Origin API
-                bf.setOriginBF(bf.getBukkitEntity().getLocation());
-            }
-       // }
-    }*/
-
-	@Inject(method = "setRespawnData", at = @At("HEAD"), cancellable = true)
-	public void setRespawnDataPaper(LevelData.RespawnData respawnData, CallbackInfo ci) {
-		// Paper start
-		if (!((LevelData_RespawnDataBridge)(Object)this.serverLevelData.getRespawnData()).cardboard$positionEquals(respawnData)) {
-			org.bukkit.Location previousLocation = this.cardboard$getWorld().getSpawnLocation();
-			this.serverLevelData.setSpawn(respawnData);
-			this.server.getPlayerList().broadcastAll(new net.minecraft.network.protocol.game.ClientboundSetDefaultSpawnPositionPacket(respawnData), this.dimension());
-			this.server.updateEffectiveRespawnData();
-			new org.bukkit.event.world.SpawnChangeEvent(this.cardboard$getWorld(), previousLocation).callEvent();
-		}
-		if (((PrimaryLevelDataBridge)this.server.overworld().serverLevelData).cardboard$getRespawnDimension() != this.dimension()) {
-			((PrimaryLevelDataBridge)this.server.overworld().serverLevelData).cardboard$setRespawnDimension(this.dimension());
-			this.server.updateEffectiveRespawnData();
-		}
-		ci.cancel();
-		// Paper end
-	}
+    @Inject(method = "setRespawnData", at = @At("HEAD"), cancellable = true)
+    public void setRespawnDataPaper(LevelData.RespawnData respawnData, CallbackInfo ci) {
+        if (!((LevelData_RespawnDataBridge)(Object)this.serverLevelData.getRespawnData()).cardboard$positionEquals(respawnData)) {
+            org.bukkit.Location previousLocation = this.cardboard$getWorld().getSpawnLocation();
+            this.serverLevelData.setSpawn(respawnData);
+            this.server.getPlayerList().broadcastAll(new net.minecraft.network.protocol.game.ClientboundSetDefaultSpawnPositionPacket(respawnData), this.dimension());
+            this.server.updateEffectiveRespawnData();
+            new org.bukkit.event.world.SpawnChangeEvent(this.cardboard$getWorld(), previousLocation).callEvent();
+        }
+        if (((PrimaryLevelDataBridge)this.server.overworld().serverLevelData).cardboard$getRespawnDimension() != this.dimension()) {
+            ((PrimaryLevelDataBridge)this.server.overworld().serverLevelData).cardboard$setRespawnDimension(this.dimension());
+            this.server.updateEffectiveRespawnData();
+        }
+        ci.cancel();
+    }
 }
